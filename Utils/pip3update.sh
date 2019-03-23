cf71be09d6354c9f7efc60577380c35d8f9d5c25
FrenchBear38 on WSL Ubuntu 18.04 on Graviton <FrenhBear38@outlook.com>
2019-03-23 09:37:40 +0100

    ski a blank line before each module update

--- Utils/pip3update.sh
+++ Utils/pip3update.sh
@@ -6,6 +6,7 @@ hostname -I | grep -q "^10.33." && PROXY="--proxy http://10.33.224.199:8080"
 
 pip3 list --outdated $PROXY | fgrep wheel | awk 'NR>2 {print $1}' | while read p
 do
+	echo
 	echo $p
 	sudo -H pip3 install --upgrade $p $PROXY
 done
